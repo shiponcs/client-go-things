0a6a3ee9a00a67ea29d9432b723cb38dc317541c
Abdul Matin <matin@appscode.com>
2025-01-02 11:42:28 +0600

    initial

---
+++ CRUD-deployment/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"k8s.io/client-go/util/retry"
+	"os"
+)
+
+func main() {
+	config, err := clientcmd.BuildConfigFromFlags("", homedir.HomeDir()+"/.kube/config")
+	if err != nil {
+		panic(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+	deploymentsClient := clientset.AppsV1().Deployments("default")
+
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "client-go-speaking",
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: GetInt32Ptr(2),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "client-go-speaking",
+				},
+			},
+			Template: v1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "client-go-speaking",
+					},
+				},
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name:  "web",
+							Image: "nginx:1.7.9",
+							Ports: []v1.ContainerPort{
+								{
+									Name:          "http",
+									Protocol:      v1.ProtocolTCP,
+									ContainerPort: 80,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// create Deployement
+	fmt.Println("Creating deployment...")
+	result, err := deploymentsClient.Create(context.TODO(), deployment, metav1.CreateOptions{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Created deployment %q.\n", result.GetObjectMeta().GetName())
+
+	prompt()
+	fmt.Println("Updating deployment...")
+	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		result, getErr := deploymentsClient.Get(context.TODO(), "client-go-speaking", metav1.GetOptions{})
+		if getErr != nil {
+			return getErr
+		}
+
+		result.Spec.Replicas = GetInt32Ptr(1)
+		result.Spec.Template.Spec.Containers[0].Image = "nginx:1.7.3"
+		_, updateErr := deploymentsClient.Update(context.TODO(), result, metav1.UpdateOptions{})
+		return updateErr
+	})
+	if retryErr != nil {
+		panic(fmt.Errorf("Update failed: %v", retryErr))
+	}
+	fmt.Printf("Updated deployment %q.\n", result.GetObjectMeta().GetName())
+
+	prompt()
+	fmt.Println("Listing deployments... in ", v1.NamespaceDefault)
+	list, err := deploymentsClient.List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	for _, d := range list.Items {
+		fmt.Printf(" * %s (%d replicas)\n", d.Name, *d.Spec.Replicas)
+	}
+	prompt()
+	fmt.Println("Deleting deployment...")
+	deletePolicy := metav1.DeletePropagationForeground
+	if err := deploymentsClient.Delete(context.TODO(), "client-go-speaking", metav1.DeleteOptions{
+		PropagationPolicy: &deletePolicy,
+	}); err != nil {
+		panic(err)
+	}
+	fmt.Printf("Deleted deployment %q.\n", result.GetObjectMeta().GetName())
+}
+
+func GetInt32Ptr(i int32) *int32 {
+	return &i
+}
+
+func prompt() {
+	fmt.Printf("-> Press Return key to continue.")
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		break
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	fmt.Println()
+}
